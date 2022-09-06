2a1dfabd4499612ab8be5fd1183cd69a7338f3d1
Bester <besster.gg@gmail.com>
2022-09-06 12:52:28 -0300

    task 9 fixed

---
+++ python-if_else_loops_functions/9-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# print_last_digit = __import__('9-print_last_digit').print_last_digit
+
+# r =print_last_digit(98)
+# print(r)
+# r =print_last_digit(0)
+# print(r)
+# r = print_last_digit(-1024)
+# print(r)
+print_last_digit = __import__('9-print_last_digit').print_last_digit
+
+print(print_last_digit(98))
