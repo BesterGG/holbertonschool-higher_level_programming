21f64d996927feeaa72ab8ad717a00660b817c19
Bester <besster.gg@gmail.com>
2022-09-12 12:58:55 -0300

    Task 6 fix spaces

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(0, len(matrix)):
         for n in range(0, len(matrix[i])):
-            if n < len(matrix[i]):
+            if n < len(matrix[n]):
                 print("{:d}".format(matrix[i][n]), end=' ')
         if i < len(matrix): 
             print()
