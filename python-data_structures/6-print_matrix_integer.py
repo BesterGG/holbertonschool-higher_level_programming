ab30d53be5c545ef5ee2995a87eecdc812dc08a6
Bester <besster.gg@gmail.com>
2022-09-12 12:54:37 -0300

    Task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-from re import I
-
-
 def print_matrix_integer(matrix=[[]]):
     for i in range(0, len(matrix)):
         for n in range(0, len(matrix[i])):
-            print("{}".format(matrix[i][n]), end=' ')
+            print("{:d}".format(matrix[i][n]), end=' ')
         print()
