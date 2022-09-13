1baa304fccce39a2c6d9b860c6a7bed14e4890a2
Bester <besster.gg@gmail.com>
2022-09-13 15:57:07 -0300

    Task 2 besterizada

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -13,7 +13,4 @@ def reduce(function, iterable, initializer=None):
 def uniq_add(my_list=[]):
     if not my_list:
         return 0
-    add = 0
-    unique = list(set(my_list))
-    add = reduce(lambda x, y: x + y, unique)
-    return add
+    return reduce(lambda x, y: x + y, list(set(my_list)))
