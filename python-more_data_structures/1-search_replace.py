5b2ce63b954a1ee0f577405d4fdac9b89b19b6e9
Bester <besster.gg@gmail.com>
2022-09-13 15:06:40 -0300

    Task1 fix

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
-# #!/usr/bin/python3
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
     res = list(map(int, list(map(lambda x: str(x).replace(str(search), str(replace)), my_list))))
     return res
