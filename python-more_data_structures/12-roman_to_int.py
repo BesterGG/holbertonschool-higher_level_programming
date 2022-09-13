02c1188249d08e724546c3c60b7405aff2a388d3
Bester <besster.gg@gmail.com>
2022-09-13 17:50:31 -0300

    Task12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    i = 0
+    num = 0
+    while i < len(roman_string):
+        if i+1 < len(roman_string) and roman_string[i:i+2] in roman:
+            num += roman[roman_string[i:i+2]]
+            i += 2
+        else:
+            num += roman[roman_string[i]]
+            i += 1
+    return num
