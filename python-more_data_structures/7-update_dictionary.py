95e27ae6bf831ba4bf2d703a134a28b38a79b02e
Bester <besster.gg@gmail.com>
2022-09-13 16:54:54 -0300

    task 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+from re import A
+
+
 def update_dictionary(a_dictionary, key, value):
-    return a_dictionary.update({key: value})
+    a_dictionary.update({key: value})
+    return a_dictionary
