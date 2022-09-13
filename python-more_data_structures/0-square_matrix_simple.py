49bb12af00d810e732044729a25e1e96bc2ed7a9
Bester <besster.gg@gmail.com>
2022-09-13 14:26:05 -0300

    Add readme

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     nm = []
     for i in matrix:
-        nm.append(list(map(lambda x:x * x, i)))
+        nm.append(list(map(lambda x: x * x, i)))
     return nm
