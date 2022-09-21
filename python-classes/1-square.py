08533b41c98bf831b238d691eb7ec21fed6d976c
Bester <besster.gg@gmail.com>
2022-09-21 20:28:00 -0300

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Square class created
+"""
+
+
+class Square:
+    """
+        Create a instance size inside square class
+    """
+    def  __init__(self, size):
+        """ Size init"""
+        self.__size = size
