aef271eeb203811e61cc53751b0236d29d1e0679
Bester <besster.gg@gmail.com>
2022-09-21 20:40:58 -0300

    Task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Square class created
+"""
+
+
+class Square:
+    """
+        Create a instance size inside square class
+    """
+
+    def __init__(self, size=0):
+        """ Size init"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
