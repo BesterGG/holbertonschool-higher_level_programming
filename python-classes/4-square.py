a9c458a43705a12522c04c253e997095dd133733
Bester <besster.gg@gmail.com>
2022-09-21 21:23:37 -0300

    tAsk4

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    Square class created
+"""
+
+
+class Square:
+    """
+        Create a instance size inside square class
+    """
+
+    def __init__(self, size=0):
+        """ Size init"""
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
