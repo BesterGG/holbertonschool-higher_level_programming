54c82c9a8584ec4030b0a1f43f1db55044413270
Bester <besster.gg@gmail.com>
2022-09-25 21:08:43 -0300

    Task1 fix

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,49 @@
 #!/usr/bin/python3
-""" class """
+""" rectangle class """
 
 
-class Square:
+class Rectangle:
     """ class attributes """
+    __width = None
+    __height = None
 
-    __size = None
-    """ init function """
-    def __init__(self, size) -> None:
-        self.__size = size
+    def __init__(self, width=0, height=0):
+        """ init function // constructor """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
