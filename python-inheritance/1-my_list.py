d238a4ec343a1a8d357c2c35c81650e3c23dcbb9
Bester <besster.gg@gmail.com>
2022-09-27 19:39:43 -0300

    Task1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,6 +7,7 @@ Task 1 - Write a class MyList that inherits from list
 class Mylist(list):
     """Class Mylist"""
     def print_sorted(self):
+        """Print sorted list"""
         sl = self.copy()
         sl.sort()
         print(sl)
