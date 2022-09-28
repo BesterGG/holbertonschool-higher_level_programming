ac4c90241576c20d1808d3d95120343f1cbb9ada
Bester <besster.gg@gmail.com>
2022-09-28 14:42:24 -0300

    Task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,6 +7,7 @@ Otherwise False
 
 
 def is_same_class(obj, a_class):
+    """Function which returns true if the object is exactly an instance of the class"""
     if isinstance(obj, a_class):
         return True
     return False
