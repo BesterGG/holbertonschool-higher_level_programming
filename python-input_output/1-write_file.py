6654a02294ec0492b978a9b6566f43b759e898fa
Bester <besster.gg@gmail.com>
2022-09-28 20:22:46 -0300

    Task1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Function that writes a string to a text file."""
+
+
+def write_file(filename="", text=""):
+    """ Writes a string to a text file."""
+    with open(filename, "w") as f:
+        f.write(text)
+        return len(text)
