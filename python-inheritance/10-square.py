ea9a85552f62981726bbc24fc1a7b13f24a903bf
Bester <besster.gg@gmail.com>
2022-09-28 16:34:13 -0300

    Task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -9,9 +9,11 @@ class Square(Rectangle):
     """Square"""
 
     def __init__(self, size):
+        """init"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__.area(size, size)
 
     def area(self):
+        """Area es area"""
         return super().area()
