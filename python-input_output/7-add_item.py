e7db0c296efb38773fce9e6dcb4069f38b549768
Bester <besster.gg@gmail.com>
2022-09-28 21:56:11 -0300

    Task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""" Write a script that adds all arguments to a python list, and then save them to a file. """
+import sys
+
+SV = __import__('5-save_to_json_file').save_to_json_file
+LD = __import__('6-load_from_json_file').load_from_json_file
+
+f = "add_item.json"
+ld = LD(f)
+for i in sys.argv:
+    f.append(i)
+SV(ld, f)
