72195f9505c2230762b9b5e318d13fedf75b56cb
Bester <besster.gg@gmail.com>
2022-09-28 23:15:09 -0300

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,27 @@
+# #!/usr/bin/python3
+# """ Write a """
+# import sys
+# import os.path
+
+# SV = __import__('5-save_to_json_file').save_to_json_file
+# LD = __import__('6-load_from_json_file').load_from_json_file
+
+# fd = "add_item.json"
+# ld = LD(fd) if os.path.exists(fd) else []
+# for i in sys.argv:
+#     fd.append(i)
+# SV(ld, fd)
+
 #!/usr/bin/python3
-""" Write a """
+""" load, add, save """
 import sys
 import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-SV = __import__('5-save_to_json_file').save_to_json_file
-LD = __import__('6-load_from_json_file').load_from_json_file
+fp = "add_item.json"
+ls = load_from_json_file(fp) if os.path.exists(fp) else []
 
-fd = "add_item.json"
-ld = LD(fd) if os.path.exists(fd) else []
 for i in sys.argv:
-    fd.append(i)
-SV(ld, fd)
-
+    ls.append(i)
+save_to_json_file(ls, fp)
