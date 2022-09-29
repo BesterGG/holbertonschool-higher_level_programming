441ccfe4cd20208c3cc919f33080a892726b15af
Bester <besster.gg@gmail.com>
2022-09-28 23:18:41 -0300

    task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,12 +2,12 @@
 """ load, add, save """
 import sys
 import os.path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+SV = __import__('5-save_to_json_file').save_to_json_file
+LD = __import__('6-load_from_json_file').load_from_json_file
 
-fp = "add_item.json"
-ls = load_from_json_file(fp) if os.path.exists(fp) else []
+fd = "add_item.json"
+ld = LD(fd) if os.path.exists(fd) else []
 
 for i in range(1, len(sys.argv)):
-    ls.append(i)
-save_to_json_file(ls, fp)
+    ld.append(i)
+SV(ld, fd)
