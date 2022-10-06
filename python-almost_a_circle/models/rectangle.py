d2874682ab69a75447c2abe4e8d3532cb69b41fd
Bester <besster.gg@gmail.com>
2022-10-06 12:45:19 -0300

    Task 2 Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Recntagle class that inherits from Base"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Recntagle class that inherits from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+        @property
+        def width(self):
+            return self._width
+
+        @width.setter
+        def width(self, value):
+            self._width = value
+
+        @property
+        def height(self):
+            return self._height
+
+        @height.setter
+        def height(self, value):
+            self.__height = value
+
+        @property
+        def x(self):
+            return self._x
+
+        @x.setter
+        def x(self, value):
+            self._x = value
+
+        @property
+        def y(self):
+            return self._y
+
+        @y.setter
+        def y(self, value):
+            self._y = value
