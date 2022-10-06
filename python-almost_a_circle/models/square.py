82da7d825be98d16683cd0ddd9bbfed0bd70a4fb
Bester <besster.gg@gmail.com>
2022-10-06 18:44:25 -0300

    Task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Square module"""
+from symbol import yield_arg
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """Size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Set size of the square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size <= 0:
+            raise ValueError("size must be > 0")
+        else:
+            self.__size = size
