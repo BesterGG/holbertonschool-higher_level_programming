501d4ebddad5505f079c11b6eb9928b972a342aa
Bester <besster.gg@gmail.com>
2022-10-06 11:31:25 -0300

    fix comment

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Base class for all"""
+
+
 class Base:
     """Base class for all objects"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
