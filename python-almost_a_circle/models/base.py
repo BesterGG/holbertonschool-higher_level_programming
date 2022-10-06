a08886b77b44de4c8f30821b655567efb0972965
Bester <besster.gg@gmail.com>
2022-10-06 11:29:10 -0300

    Task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Base class for all"""
+class Base:
+    __nb_objects = 0
+    """Base class for all objects"""
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
