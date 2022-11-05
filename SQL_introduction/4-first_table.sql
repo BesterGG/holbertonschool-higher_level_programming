841c12976443ab5009ea10953b4e5d10832e3a19
Jose Alvarez <5135@holbertonschool.com>
2022-11-05 10:07:10 -0700

    t4 fix

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Create firts table in the current db
-CREATE TABLE IF NOT EXISTS first_table(id INT NOT NULL, name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table(id INT NOT NULL, name VARCHAR(256));
