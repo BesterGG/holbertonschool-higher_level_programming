a8ab531dcc561ec91370d0c52d691d700bd3561c
Jose Alvarez <5135@holbertonschool.com>
2022-11-05 21:12:12 -0700

    task9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Inner join
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = state.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = state_id ORDER BY cities.id ASC;
