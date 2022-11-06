77e8b10ed63aaacb0732c974982bab4a654f795e
Jose Alvarez <5135@holbertonschool.com>
2022-11-05 20:39:50 -0700

    task8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Lists all cities of california that can be found in the db
-SELECT id, name FROM cities WHERE states_id IN (SELECT name FROM states WHERE name = 'California');
+SELECT id, name FROM cities WHERE id IN (SELECT name FROM states WHERE name = 'California');
