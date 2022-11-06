bbe5e6c5aee77e152de6c0e84b1deca64ab44b7d
Jose Alvarez <5135@holbertonschool.com>
2022-11-05 21:11:15 -0700

    task9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Inner join
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = state.id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = state.id ORDER BY cities.id ASC;
