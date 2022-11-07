d9a5af465e87a862527da635f1f9e80ef761f7f9
Bester <besster.gg@gmail.com>
2022-11-07 18:39:11 -0300

    Task2 fixed

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -4,52 +4,20 @@ Select all lists from the database hbtn_0e_0usa
 '''
 import MySQLdb
 import sys
-
-
-class DataBase:
-    '''
-    Class representing connection to MySQL database
-    '''
-    def __init__(
-                self, arg_user, arg_password, arg_database,
-                arg_name_searched
-                ):
-        self.user = arg_user
-        self.password = arg_password
-        self.database = arg_database
-        self.name_searched = arg_name_searched
-        db = MySQLdb.Connect(
-                            host='localhost',
-                            port=3306,
-                            user=self.user,
-                            password=self.password,
-                            db=self.database
-                            )
-        self.cur = db.cursor()
-
-    def print_all_states(self):
-        self.cur.execute("SELECT * FROM states")
-        all_states = self.cur.fetchall()
-        for states in all_states:
-            if states[1][0] == "N":
-                print(f"({states[0]}, '{states[1]}')")
-
-    def name_to_search(self, name_searched):
-        search_consult = """SELECT * FROM states WHERE BINARY name='{}'
-                            ORDER BY id ASC""".format(name_searched)
-        self.cur.execute(search_consult)
-        all_name_searched = self.cur.fetchall()
-        for name_db in all_name_searched:
-            print("{}".format(name_db))
-
+from sys import argv
 
 if __name__ == "__main__":
-    args = sys.argv
-    arg_user = args[1]
-    arg_password = args[2]
-    arg_database = args[3]
-    arg_name_searched = args[4]
-    all_states = DataBase(
-                        arg_user, arg_password, arg_database, arg_name_searched
-                        )
-    all_states.name_to_search(arg_name_searched)
+    db = MySQLdb.Connect(
+                    host='localhost',
+                    port=3306,
+                    user=argv[1],
+                    password=argv[2],
+                    db=argv[3]
+                    )
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE BINARY name='{:s}'ORDER BY id ASC"
+        .format(argv[4])
+        )
+    for row in cursor.fetchall():
+            print("{}".format(row))
