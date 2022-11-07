de0023a7ecad95440b442cc251a252becd6a6341
Bester <besster.gg@gmail.com>
2022-11-07 17:54:23 -0300

    task1 and 2 permissions

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,10 +10,11 @@ class DataBase:
     '''
     Class representing connection to MySQL database
     '''
-    def __init__(self, arg_user, arg_password, arg_database):
+    def __init__(self, arg_user, arg_password, arg_database, arg_name_searched):
         self.user = arg_user
         self.password = arg_password
         self.database = arg_database
+        self.name_searched = arg_name_searched
         db = MySQLdb.Connect(
                             host='localhost',
                             port=3306,
@@ -30,11 +31,19 @@ class DataBase:
             if states[1][0] == "N":
                 print(f"({states[0]}, '{states[1]}')")
 
+    def name_to_search(self, name_searched):
+        search_consult = "SELECT * FROM states WHERE name='{}'".format(name_searched)
+        self.cur.execute(search_consult)
+        all_name_searched = self.cur.fetchall()
+        for name_db in all_name_searched:
+                print("{}".format(name_db))
+
 
 if __name__ == "__main__":
     args = sys.argv
     arg_user = args[1]
     arg_password = args[2]
     arg_database = args[3]
-    all_states = DataBase(arg_user, arg_password, arg_database)
-    all_states.print_all_states()
+    arg_name_searched = args[4]
+    all_states = DataBase(arg_user, arg_password, arg_database, arg_name_searched)
+    all_states.name_to_search(arg_name_searched)
