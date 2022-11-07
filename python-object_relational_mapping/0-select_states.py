e05fd9dd2bb7fa69b893e2adc11e556b2148ca2e
Bester <besster.gg@gmail.com>
2022-11-07 16:59:15 -0300

    PEP8

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -15,12 +15,12 @@ class DataBase:
         self.password = arg_password
         self.database = arg_database
         db = MySQLdb.Connect(
-            host = 'localhost',
-            port = 3306,
-            user = self.user,
-            password = self.password,
-            db = self.database
-        )
+                            host='localhost',
+                            port=3306,
+                            user=self.user,
+                            password=self.password,
+                            db=self.database
+                            )
         self.cur = db.cursor()
 
     def print_all_states(self):
@@ -28,6 +28,7 @@ class DataBase:
         all_states = self.cur.fetchall()
         for states in all_states:
             print(f"({states[0]}, '{states[1]}')")
+
 
 if __name__ == "__main__":
     args = sys.argv
