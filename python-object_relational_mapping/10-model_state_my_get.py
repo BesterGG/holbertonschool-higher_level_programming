c8f27bcc236f81836c141fc7ab4955d333c3bc8f
Jose Alvarez <5135@holbertonschool.com>
2022-11-08 12:43:27 -0800

    Task10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''''Lists all State objects from the database hbtn_0e_6_usa'''
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+    SQ = session.query(State).filter(State.name == argv[4])
+
+    if SQ:
+        for sq in SQ:
+            print(f"{sq.id}")
+    else:
+        print("Not found")
