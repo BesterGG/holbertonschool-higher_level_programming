f0a1e13bb1b4e3893904e0a64e606c1f80e6afa5
Jose Alvarez <5135@holbertonschool.com>
2022-11-09 07:44:00 -0800

    Task13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''''Lists all State objects from the database hbtn_0e_6_usa'''
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import update
+
+if __name__ == "__main__":
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    SQ = session.query(State).all()
+    for sq in SQ:
+        if "a" in sq.name:
+            session.delete(sq)        
+    session.commit()
+    session.close()
