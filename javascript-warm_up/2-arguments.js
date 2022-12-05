c683e83b816f3fe0fd4ee844e9f00e7617fbf5a1
Bester <besster.gg@gmail.com>
2022-12-05 17:40:18 -0300

    semistandard

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,7 +2,9 @@
 const myArgs = process.argv;
 const len = myArgs.length - 1;
 
-if (len > 1) {
+if (len > 1 & len < 2) {
+    console.log('Argument found');
+} else if (len > 2) {
   console.log('Arguments found');
 } else {
   console.log('No argument');
