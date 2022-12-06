301cf6336ede0aa0d82b11231873220d0b07fdaa
Bester <besster.gg@gmail.com>
2022-12-06 12:49:20 -0300

    Task11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
 
 const argv = process.argv.sort();
-const large = argv.length
-let i, top = 0, second = 0;
+const large = argv.length;
+let i;
+let top = 0;
+let second = 0;
 
-for (i = 0;i < large;i++) {
-  if (argv[i] > top){
-      top = argv[i];
+for (i = 0; i < large; i++) {
+  if (Number(argv[i]) > top) {
+    top = Number(argv[i]);
+  }
 }
-}
-for (i = 0;i < large;i++) {
-  if (argv[i] > second && argv[i] < top){
-    second = argv[i];  
+for (i = 0; i < large; i++) {
+  if (Number(argv[i]) > second && Number(argv[i]) < top) {
+    second = Number(argv[i]);
   }
 }
 console.log(second);
