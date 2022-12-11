6ef91e521c82f85313d76c71759e6924b7203a57
Bester <besster.gg@gmail.com>
2022-12-11 11:48:42 -0300

    T4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let row = '';
+    let col = 0;
+
+    // Build a row of X characters
+    while (col < this.width) {
+      row += 'X';
+      col++;
+    }
+
+    // Print the rows
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.height * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
