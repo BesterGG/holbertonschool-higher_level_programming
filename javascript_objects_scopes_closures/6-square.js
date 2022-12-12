ae2513e10cdea3a7b49831dabc42c05c3958c2aa
Bester <besster.gg@gmail.com>
2022-12-11 22:54:03 -0300

    T6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const SQ = require('./5-square');
+class Square extends (SQ) {
+  charPrint (C) {
+    let row = '';
+    let col = 0;
+    while (col < this.width) {
+      if (C) {
+        row += C;
+        col++;
+      } else {
+        row += 'X';
+        col++;
+      }
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(row)
+    }
+  }
+}
+module.exports = Square;
