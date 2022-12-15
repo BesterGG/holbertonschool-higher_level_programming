f1da03876c64ea6f93f103a08064606bdc5cc5b2
Bester <besster.gg@gmail.com>
2022-12-15 16:39:45 -0300

    T2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request.get(url, (err,res, body) => {
-    if (err) {
-        console.error(err);
-        return
-    }
-    console.log(`code: ${res.statusCode}`);
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`code: ${res.statusCode}`);
 });
