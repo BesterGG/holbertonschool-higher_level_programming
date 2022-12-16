74cacb9e4284916894ddcbfdcf917ef62fcd9689
Bester <besster.gg@gmail.com>
2022-12-15 21:24:19 -0300

    Task4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = process.argv[2];
 request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const data = JSON.parse(body).results;
