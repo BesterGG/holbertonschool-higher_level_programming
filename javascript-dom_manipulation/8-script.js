543ff9ba939d236e5f121cb0eba671002e2cdc7f
Bester <besster.gg@gmail.com>
2022-12-18 17:42:09 -0300

    T8

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  fetch('https://stefanbohacek.com/hellosalut/?lang=fr')
+  .then(response => response.json())
+  .then(data => {
+    const hi = document.querySelector('#hello');
+    hi.textContent = data.hello;
+  });
+}
